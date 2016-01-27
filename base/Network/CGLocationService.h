54494e787c8c1da221fffd2a1a7c21d18ca9572f
rsaif <wangshuguan@gmail.com>
2016-01-27 12:54:45 +0800

    加个注释

--- base/Network/CGLocationService.h
+++ base/Network/CGLocationService.h
@@ -21,10 +21,10 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 typedef NS_ENUM(NSUInteger, CGLocationError){
-    CGLocationErrorNoPermision,
-    CGLocationErrorDenied,
-    CGLocationErrorNoDetermined,
-    CGLocationErrorNormal
+    CGLocationErrorNoPermision, //没有开启定位服务
+    CGLocationErrorDenied,      //被拒绝的权限
+    CGLocationErrorNoDetermined,//未曾选择的权限
+    CGLocationErrorNormal       //访问正常
 };
 @interface CGLocationData:NSObject
 @property(nonatomic,strong)NSString *address;
@@ -32,10 +32,18 @@ typedef NS_ENUM(NSUInteger, CGLocationError){
 +(id)locationDataWithAddress:(NSString *)address location:(CLLocation *)location;
 @end
 @interface CGLocationService : NSObject<UIAlertViewDelegate>
+/*!
+ @brief 启动定位
+ */
 -(void)startLocation;
+/*!
+ @brief 如果你使用这种启动方式，则不会立即调用startLocation，你需要手动去调用
+ 
+ @return
+ */
 - (instancetype)initWithoutGetLocation;
 /*!
- @brief 获取授权事件
+ @brief 获取授权事件，你可以不写，他会有弹框提供选择
  */
 @property(nonatomic,strong)void(^warrantAction)(CGLocationError error);
 @end
